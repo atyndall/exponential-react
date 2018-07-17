195d11ae77458dfdda5c574e3211d50bfaa3a86f
Ash Tyndall <git@atyndall.net>
2018-07-17 11:22:58 +0200

    More improvements

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import numeral from 'numeral';
 import Indicator from './Indicator.js';
 import './App.css';
 
-
 const PERCENTAGES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50];
 const YEARS       = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 const DOLLARS     = [100];
 
 const calculateAnswer = (percentage, years, dollars) => (Math.round(dollars * Math.pow(1 + (percentage/100), years)));
 
-const generateRandomBlankState = () => ({
+const generateBlankState = () => ({
   dollars: _.sample(DOLLARS),
   percent: _.sample(PERCENTAGES),
   years: _.sample(YEARS),
@@ -23,7 +22,7 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = generateRandomBlankState();
+    this.state = generateBlankState();
 
     this.handleReset  = this.handleReset.bind(this);
     this.handleTry    = this.handleTry.bind(this);
@@ -31,7 +30,7 @@ class App extends Component {
   }
 
   handleReset() {
-    this.setState(generateRandomBlankState());
+    this.setState(generateBlankState());
   }
 
   handleTry() {
@@ -44,7 +43,6 @@ class App extends Component {
 
   render() {
     const {dollars, percent, years, answer, submitted} = this.state;
-    const answerNumber = answer.value();
 
     return (
       <div className="App">
@@ -54,10 +52,10 @@ class App extends Component {
           <div className="text-input">
             Answer: $<input type="text" id="text-input" name="lname" value={answer.format('0,0')} onChange={this.handleChange} /><br/>
           </div>
-          <button type="submit" id="enter-button" onClick={this.handleTry} disabled={isNaN(answerNumber)}>Try</button>
+          <button type="submit" id="enter-button" onClick={this.handleTry} disabled={isNaN(answer.value())}>Try</button>
           <button type="button" id="reset-button" onClick={this.handleReset}>Reset</button>
         </div>
-        {submitted && <Indicator correctAnswer={calculateAnswer(percent, years, dollars)} yourAnswer={answerNumber} />}
+        {submitted && <Indicator correctAnswer={calculateAnswer(percent, years, dollars)} yourAnswer={answer.value()} />}
       </div>
     );
   }
--- src/Indicator.js
+++ src/Indicator.js
@@ -3,24 +3,26 @@ import numeral from 'numeral';
 
 const Indicator = (props) => {
   const {correctAnswer, yourAnswer} = props;
-  const difference = Math.abs(yourAnswer - correctAnswer);
+
+  const difference      = Math.abs(yourAnswer - correctAnswer);
   const formattedAnswer = numeral(correctAnswer).format('0,0');
 
+  let color, status;
+
   if (difference < (0.01 * correctAnswer)) {
-    return (
-      <p id="indicator" style={{color: 'green'}}>Correct, the answer is {formattedAnswer}</p>
-    );
-
+    color  = 'green';
+    status = 'Correct';
   } else if (difference < (0.2 * correctAnswer)) {
-    return (
-      <p id="indicator" style={{color: 'green'}}>Close enough, the answer is {formattedAnswer}</p>
-    );
-
+    color  = 'green';
+    status = 'Close enough';
   } else {
-    return (
-      <p id="indicator" style={{color: 'red'}}>Incorrect, the answer is {formattedAnswer}</p>
-    )
+    color  = 'red';
+    status = 'Incorrect';
   }
+
+  return (
+    <p id="indicator" style={{color: color}}>{status}, the answer is {formattedAnswer}</p>
+  );
 }
 
 export default Indicator;
